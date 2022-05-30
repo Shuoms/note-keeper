9e3fb2ba0e72a93b3449a04bda15dbfe072d749f
Haodong Ju <juhaodong@gmail.com>
2022-05-30 16:52:22 +0200

    转用mailtoForm

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,26 +2,29 @@
   <v-container>
     <v-row>
       <v-col cols="12">
-        <div class="pa-4" style="min-height: calc(100vh - 172px)">
-          <h2>新建笔记</h2>
-          <v-card elevation="0" color="#f6f6f6" class="mt-4">
-            <v-img width="100%"
-                   height="196px"
-                   contain
-                   v-if="uploadUrl" :src="uploadUrl"></v-img>
-          </v-card>
-          <v-file-input
-              v-model="file"
-              prepend-icon="mdi-image"
-              accept="image/*"
-              label="上传图片"
-          />
-          <v-text-field v-model="title" label="标题"></v-text-field>
-          <v-textarea label="内容" v-model="content" counter="500"></v-textarea>
-        </div>
-        <v-btn @click="sendMail" block dark large elevation="0" class="mt-4">发送
-          <v-icon right>mdi-send</v-icon>
-        </v-btn>
+        <form action="mailto:juhaodong@gmail.com" method="post" enctype="multipart/form-data">
+          <div class="pa-4" style="min-height: calc(100vh - 172px)">
+            <h2>新建笔记</h2>
+            <v-card elevation="0" color="#f6f6f6" class="mt-4">
+              <v-img width="100%"
+                     height="196px"
+                     contain
+                     v-if="uploadUrl" :src="uploadUrl"></v-img>
+            </v-card>
+            <v-file-input
+                name="file"
+                v-model="file"
+                prepend-icon="mdi-image"
+                accept="image/*"
+                label="上传图片"
+            />
+            <v-text-field name="title" v-model="title" label="标题"></v-text-field>
+            <v-textarea label="内容" name="content" v-model="content" counter="500"></v-textarea>
+          </div>
+          <v-btn name="submit" block dark large elevation="0" class="mt-4" type="submit">发送
+            <v-icon right>mdi-send</v-icon>
+          </v-btn>
+        </form>
       </v-col>
 
 
@@ -44,10 +47,6 @@ export default {
     content: ''
   }),
   methods: {
-    async sendMail () {
-      const file = await toBase64(this.file)
-      window.open('mailto:test@example.com?subject=' + this.title + '&body=' + this.content + '<img src="' + file + '"/>')
-    }
   },
   computed: {
     uploadUrl: function () {
