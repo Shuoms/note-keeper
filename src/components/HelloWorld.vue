6fa26fbc3e596e1d348c03b87be2c8416ac6005b
Haodong Ju <juhaodong@gmail.com>
2022-05-30 22:05:13 +0200

    gx

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,29 +2,32 @@
   <v-container>
     <v-row>
       <v-col cols="12">
-        <form action="mailto:juhaodong@gmail.com" method="post" enctype="multipart/form-data">
-          <div class="pa-4" style="min-height: calc(100vh - 172px)">
-            <h2>新建笔记</h2>
-            <v-card elevation="0" color="#f6f6f6" class="mt-4">
-              <v-img width="100%"
-                     height="196px"
-                     contain
-                     v-if="uploadUrl" :src="uploadUrl"></v-img>
-            </v-card>
-            <v-file-input
-                name="file"
-                v-model="file"
-                prepend-icon="mdi-image"
-                accept="image/*"
-                label="上传图片"
-            />
-            <v-text-field name="subject" v-model="title" label="标题"></v-text-field>
-            <v-textarea label="内容" name="body" v-model="content" counter="500"></v-textarea>
-          </div>
-          <v-btn name="submit" block dark large elevation="0" class="mt-4" type="submit">发送
-            <v-icon right>mdi-send</v-icon>
-          </v-btn>
-        </form>
+
+        <div class="pa-4" style="min-height: calc(100vh - 172px)">
+          <h2>新建笔记</h2>
+          <v-card elevation="0" color="#f6f6f6" class="mt-4">
+            <v-img width="100%"
+                   height="196px"
+                   contain
+                   v-if="uploadUrl" :src="uploadUrl"></v-img>
+          </v-card>
+          <v-file-input
+              name="file"
+              v-model="file"
+              prepend-icon="mdi-image"
+              accept="image/*"
+              label="上传图片"
+          />
+          <v-text-field name="subject" v-model="title" label="标题"></v-text-field>
+          <v-textarea label="内容" name="body" v-model="content" counter="500"></v-textarea>
+        </div>
+        <v-btn v-if="linkReady" download="message.eml" ref="link"  block color="primary" large elevation="0" class="mt-4">下载并发送</v-btn>
+        <v-btn v-else @click="sendMail" block dark large elevation="0" class="mt-4">
+          生成
+          <v-icon right>mdi-send</v-icon>
+        </v-btn>
+
+
       </v-col>
 
 
@@ -34,14 +37,67 @@
 
 <script>
 
+import { hillo } from 'hillo'
+
+const baseUrl = "https://aaden.online/email/"
+
+async function uploadImg (file) {
+  return await hillo.postWithUploadFile(baseUrl + "demo.php", {file})
+}
+
+
+const makeTextFile = function (text) {
+  const data = new Blob([text], {type: 'text/plain'})
+  let textFile = null
+  textFile = window.URL.createObjectURL(data)
+  return textFile
+}
+
 
 export default {
   data: () => ({
     file: null,
     title: '',
-    content: ''
+    content: '',
+    imgUrl: '',
+    linkReady: false
   }),
   methods: {
+    sendMail () {
+      this.linkReady = true
+      this.$nextTick(() => {
+        const text = (`
+To: User <juhaodong@gmail.com>
+Subject: Subject
+X-Unsent: 1
+Content-Type: text/html
+
+<html>
+<head>
+<style>
+    body, html, table {
+        font-family: Calibri, Arial, sans-serif;
+    }
+</style>
+</head>
+<body>
+<h1>${this.title}</h1>
+${this.content}<br>
+<img width="200" height="200" src='${this.imgUrl}' alt="img"/>
+</body>
+</html>
+`)
+        this.$refs.link.href = makeTextFile(text)
+      })
+
+    }
+  },
+  watch: {
+    async file (val) {
+      const currentUrl = baseUrl + (await uploadImg(val))
+      console.log(currentUrl)
+      this.imgUrl = currentUrl
+    }
   },
   computed: {
     uploadUrl: function () {
