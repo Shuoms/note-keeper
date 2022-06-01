8e11cb8101a171aa07862931bebaf7824d8c79f5
Shuoms <mashuoshuo10@gmail.com>
2022-06-01 02:23:21 +0200

    Deutsch-2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,7 +12,7 @@
                    v-if="uploadUrl" :src="uploadUrl"></v-img>
           </v-card>
           <v-file-input
-              name="file"
+              name="file"    
               v-model="file"
               prepend-icon="mdi-image"
               accept="image/*"
@@ -21,9 +21,9 @@
           <v-text-field name="subject" v-model="title" label="Ideentitel"></v-text-field>
           <v-textarea label="Ideenbeschreibung" name="body" v-model="content" counter="500"></v-textarea>
         </div>
-        <v-btn v-if="linkReady" ref="link" block color="primary" large elevation="0" class="mt-4">下载并发送</v-btn>
+        <v-btn v-if="linkReady" ref="link" block color="primary" large elevation="0" class="mt-4">Unterladen und Absenden</v-btn>
         <v-btn v-else @click="sendMail" block dark large elevation="0" class="mt-4">
-          生成
+          Absenden
           <v-icon right>mdi-send</v-icon>
         </v-btn>
 
@@ -51,7 +51,7 @@ export default {
   methods: {
     sendMail () {
       this.$nextTick(() => {
-        window.open(`mailto:Haodong JU<juhaodong@gmail.com>?subject=${this.title}&body=${this.content}\n
+        window.open(`mailto:Shuo Ma<shuo.ma.ma@bmw.de>?subject= "Ideetitel": ${this.title}&body= "Ideebeschreibung": ${this.content}\n
 `)
       })
 
